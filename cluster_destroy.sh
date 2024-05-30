5ba761237039bbdd6ab45a9ce444bd274538bdc1
Tom Dean <tdean@d2iq.com>
2024-05-30 18:59:09 -0500

    Updated cluster_delete.sh script.

--- cluster_destroy.sh
+++ cluster_destroy.sh
@@ -4,11 +4,11 @@
 # https://github.com/BuoyantIO/hazl-orders-playground
 # Automates cluster deletion and cleans up the hazl kubectl context
 # Tom Dean | Buoyant
-# Last edit: 5/28/2024
+# Last edit: 5/30/2024
 
 # Remove the k3d cluster
 
-k3d cluster delete demo-cluster-orders-hazl
+k3d cluster delete hazl-orders-playground
 k3d cluster list
 
 # Remove the kubectl context
