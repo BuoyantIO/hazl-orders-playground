e7e4b8b83a1965c4504924c7ae26da24008293c5
Tom Dean <tdean@d2iq.com>
2024-05-30 19:34:06 -0500

    Work on ingress for Grafana and Kubecost.

--- cluster_setup.sh
+++ cluster_setup.sh
@@ -213,7 +213,8 @@ helm install kubecost cost-analyzer \
 --namespace kubecost --create-namespace \
 --set kubecostToken="dG9tQGJ1b3lhbnQuaW8=xm343yadf98"
 
-kubectl apply -f kubecost-ingress.yaml
+kubectl apply -f ext-services.yaml
+kubectl apply -f hazl-orders-playground-ingress.yaml
 
 # Deploy the Orders application to both clusters
 # Press CTRL-C to exit each watch command
