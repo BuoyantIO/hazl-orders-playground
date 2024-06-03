cc85eff7e4fe1f038608d7c828b6f344c3ab0cb8
Tom Dean <tdean@d2iq.com>
2024-06-03 14:51:34 -0500

    Added cluster_setup_naked for naked clusters.

---
+++ cluster_setup_naked.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# cluster_setup_naked.sh
+# Demo script for the hazl-orders-playground GitHub repository
+# https://github.com/BuoyantIO/hazl-orders-playground
+# Automates cluster creation, no Linkerd installation
+# Tom Dean | Buoyant
+# Last edit: 6/3/2024
+
+# Let's set some variables!
+
+# BEL: Stable
+BEL_VERSION=enterprise-2.15.3
+CLI_VERSION=install
+
+# BEL: Preview
+# BEL_VERSION=preview-24.5.4
+# CLI_VERSION=install-preview
+
+# Create the k3d clusters
+
+k3d cluster delete hazl-orders-playground
+k3d cluster create -c cluster/hazl-orders-playground.yaml --wait
+k3d image import hatoo/oha:latest -c hazl-orders-playground
+k3d cluster list
+
+# Configure the kubectl context
+
+kubectx -d hazl
+kubectx hazl=k3d-hazl-orders-playground
+kubectx hazl
+kubectx
+
+# Create fresh root certificates for mTLS
+
+cd certs
+rm -f *.{crt,key}
+step certificate create root.linkerd.cluster.local ca.crt ca.key \
+--profile root-ca --no-password --insecure
+step certificate create identity.linkerd.cluster.local issuer.crt issuer.key \
+--profile intermediate-ca --not-after 8760h --no-password --insecure \
+--ca ca.crt --ca-key ca.key
+ls -la
+cd ..
+
+# Read in license, Buoyant Cloud and cluster name information from the settings.sh file
+
+source settings.sh
+
+# Install the CLI
+
+curl https://enterprise.buoyant.io/$CLI_VERSION | sh
+export PATH=~/.linkerd2/bin:$PATH
+linkerd version
+
+exit 0
